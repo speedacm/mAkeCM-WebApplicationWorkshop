7d08f4eef7efd56aa9a6ce8a4e9869cb171f9b07
luke samuel <gky-lsamuel@nucor.com>
2019-06-28 15:43:47 -0400

    Created Angular Project

---
+++ my-portfolio/src/app/data/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  // Functions for loading and sending data to the API
+}
+
+const api = {
+  url: 'https://caswell.dev/mAkeCM/',
+
+  // TODO: Replace the value READONLY with the key given to you at the beginning of the workshop
+  key: 'READONLY'
+
+}
